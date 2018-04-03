3059b416f99e0ee314e0e1ee68d0ed9491c86778
Ben Basuni <benbasuni1@gmail.com>
2018-04-03 14:40:05 -0700

    server side

--- server/index.js
+++ server/index.js
@@ -4,11 +4,7 @@ const router     = require('./routes.js');
 
 // SETUP
 const app = express();
-console.log(`process.env.NODE_ENV: `, process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-  console.log(`i am here`);
-  console.log(__dirname + '/../build');
-  app.use(express.static(__dirname + '/../build'));
-}
+if (process.env.NODE_ENV === 'production') 
+  app.use(express.static(__dirname + '/../build/'));
 app.use(router);
 app.listen(process.env.PORT || 3001);
