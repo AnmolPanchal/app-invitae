2eefa1cb5b2e0013be5e95c0f3b5e17ec56a0dee
Ben Basuni <benbasuni1@gmail.com>
2018-04-03 14:36:56 -0700

    testing server.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const router     = require('./routes.js');
 
 // SETUP
 const app = express();
-if (process.env.NODE_ENV === 'production') 
+console.log(`process.env.NODE_ENV: `, process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  console.log(`i am here`);
+  console.log(__dirname + '/../build');
   app.use(express.static(__dirname + '/../build'));
+}
 app.use(router);
 app.listen(process.env.PORT || 3001);
